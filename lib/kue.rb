9abd9aaa9ef4d17033f86f1b06126ec06b26a6b1
Dominik Bamberger <dmayer@suse.de>
2012-04-27 15:32:47 +0200

    Avoid deprecation warnings for set_table_name and set_primary_key.

--- lib/kue.rb
+++ lib/kue.rb
@@ -5,35 +5,35 @@ module Kue
   module Store
     def self.included(base)
       base.send :extend, ClassMethods
-      base.send :set_table_name, :kue_settings
-      base.send :set_primary_key, :key
+      base.send :table_name=, :kue_settings
+      base.send :primary_key=, :key
     end
-  
-    module ClassMethods         
+
+    module ClassMethods
       def keys
         KueStore.all.map(&:key).map(&:to_sym)
       end
-  
+
       def [](key)
         entry = KueStore.find_by_key(key)
         entry ? YAML.load(entry.value) : nil
       end
-       
+
       def []=(key, value)
         raise KueNilKeyError if key.nil?
         setting = KueStore.find_or_create_by_key(key)
         setting.value = value.to_yaml
         setting.save!
       end
-  
+
       def delete!(key)
         KueStore.delete_all(:key => key)
       end
-  
+
       def exists?(key)
         self[key].present?
       end
-      
+
       def clear!
         KueStore.destroy_all
       end
