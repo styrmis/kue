f17356531e3411567d9362e4d861dd17352ac3b9
Dominik Bamberger <dmayer@suse.de>
2012-04-27 15:45:29 +0200

    Fix complex object test.

--- spec/kue_spec.rb
+++ spec/kue_spec.rb
@@ -4,46 +4,53 @@ describe KueStore do
   it 'should have the kue_settings table correctly setup' do
     ActiveRecord::Base.connection.table_exists?(:kue_settings).should be_true
   end
-  
+
   it 'should save a new key and value' do
     proc { KueStore[:key_one] = "Key One"}.should_not raise_error
   end
-  
+
   it 'should retrieve a keys value' do
     KueStore[:key_two] = "Key Two"
     KueStore[:key_two].should == "Key Two"
   end
-  
+
   it 'should save and retrieve complex objects' do
-    proc { KueStore[:key_three] = Math }.should_not raise_error
-    KueStore[:key_three].sqrt(100).should == 10
+    # Build a new class to have something to store and retrieve.
+    class Foo
+      attr_accessor :bar
+    end
+    foo = Foo.new
+    foo.bar = 42
+    # Store and retrieve the instance.
+    proc { KueStore[:key_three] = foo }.should_not raise_error
+    KueStore[:key_three].bar.should == 42
   end
-  
+
   it 'should check for existance' do
     KueStore.exists?(:non_existant).should be_false
 
     KueStore[:existant] = 109
     KueStore.exists?(:existant).should be_true
   end
-  
+
   it 'should delete a key and value' do
     KueStore[:delete_me] = 1098
     KueStore.exists?(:delete_me).should be_true
     KueStore.delete!(:delete_me).should be_true
     KueStore.exists?(:delete_me).should be_false
   end
-  
+
   it 'should not throw an error when deleting a key that does not exist' do
     proc { KueStore.delete!(:non_existant) }.should_not raise_error
   end
-  
+
   it 'should list all keys' do
     KueStore[:k1] = 1
     KueStore[:k2] = 2
     KueStore[:k3] = 3
     KueStore.keys.should == [:k1, :k2, :k3]
   end
-  
+
   it 'should clear the store' do
     KueStore[:k1] = 1
     KueStore[:k2] = 2
@@ -51,18 +58,18 @@ describe KueStore do
     KueStore.clear!
     KueStore.keys.should be_empty
   end
-  
+
   it 'should count all items in the store' do
     KueStore[:k1] = 1
     KueStore[:k2] = 2
     KueStore[:k3] = 3
     KueStore.count.should == 3
   end
-  
+
   it 'should fail hard if a nil key is passed to []=' do
     proc { KueStore[nil] = 1 }.should raise_error(Kue::KueNilKeyError)
   end
-  
+
   it 'should return nil if a nil key is passed to []' do
     KueStore[nil].should be_nil
   end
@@ -72,7 +79,7 @@ describe 'Using Kue::Store outside of the KueStore class - introducing BlueStore
   class BlueStore < ActiveRecord::Base
     include Kue::Store
   end
-  
+
   it 'should be able to be used as a kue store' do
     BlueStore[:ok] = "One"
     BlueStore[:ok].should == "One"
